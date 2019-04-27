ba59d7a8ac0b9ef37403859a3fac8eb2deaa1a48
Holly <snowflake@Hollys-MacBook-Pro.local>
2019-04-27 13:50:54 -0400

    Final commit

---
+++ db/seeds.sql
@@ -0,0 +1,12 @@
+USE burgers_db;
+
+INSERT INTO burgers (burger_name, devoured) VALUES ('New Bacon-Ings', FALSE);
+INSERT INTO burgers (burger_name, devoured) VALUES ('The Child Molester', FALSE);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Foot Feta-Ish', FALSE);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Tunami', FALSE);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Mission A-Corn-Plished', FALSE);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Rest in Peas', FALSE);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Fig-Eta Bout It', FALSE);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Chevre Which Way but Loose', FALSE);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Roquefort Files', FALSE);
+INSERT INTO burgers (burger_name, devoured) VALUES ('These Collards Do not run', FALSE);
